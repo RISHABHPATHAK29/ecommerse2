876adb2631688674e570833226f71461458f7d23
RISHABH KUMAR PATHAK <132684402+RISHABHPATHAK29@users.noreply.github.com>
2026-03-13 12:13:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const orderRoutes = require("./routes/orders")
 app.use("/api/products", productRoutes)
 app.use("/api/orders", orderRoutes)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
