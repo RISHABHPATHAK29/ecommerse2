6fd7c77dc604ac8032866da5cab130199f754c85
RISHABH KUMAR PATHAK <132684402+RISHABHPATHAK29@users.noreply.github.com>
2026-03-13 11:04:53 +0530

    Refactor server to use environment variable for port

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ const orderRoutes = require("./routes/orders")
 app.use("/api/products", productRoutes)
 app.use("/api/orders", orderRoutes)
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
 
-console.log("Server running on port 5000")
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 })
